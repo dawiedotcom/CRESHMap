8818947244c253379c0e5a82332c5a8b2d4d1a56
mhagdorn <magnus.hagdorn@marsupium.org>
2022-05-19 15:42:24 +0100

    start project

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    """Set Flask config variables."""
+
+    FLASK_ENV = 'development'
+    TESTING = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'not-so-secret'
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
