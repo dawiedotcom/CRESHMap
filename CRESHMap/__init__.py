84572cbadbf36940dd720d420694a26fc0e0855c
Magnus Hagdorn <Magnus.Hagdorn@marsupium.org>
2022-05-20 12:53:45 +0100

    try adding an image

--- CRESHMap/__init__.py
+++ CRESHMap/__init__.py
@@ -1,14 +1,21 @@
 """Initialize Flask app."""
-from flask import Flask
+from flask import Flask, render_template_string
 from flask_flatpages import FlatPages
+from flask_flatpages.utils import pygmented_markdown
 
 pages = FlatPages()
+
+
+def renderer(text):
+    prerendered_body = render_template_string(text)
+    return pygmented_markdown(prerendered_body)
 
 
 def init_app():
     """Construct core Flask application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
+    app.config['FLATPAGES_HTML_RENDERER'] = renderer
 
     with app.app_context():
         # Import parts of our core Flask app
--- CRESHMap/pages/hello.md
+++ CRESHMap/pages/hello.md
@@ -2,3 +2,5 @@ title: Hello
 published: 2010-12-22
 
 *Hello*, **world**
+
+![image]({{ url_for('static', filename="images/centralbelt_density.png") }})
