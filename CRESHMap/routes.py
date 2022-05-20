43b07e72092bc72834f77b20e7d39e75fb70690d
Magnus Hagdorn <Magnus.Hagdorn@marsupium.org>
2022-05-20 17:19:55 +0100

    use bootstrap and create menu

--- CRESHMap/routes.py
+++ CRESHMap/routes.py
@@ -5,14 +5,21 @@ from flask import current_app as app
 pages = FlatPages(app)
 
 
+def menu_items():
+    menu = []
+    for p in pages:
+        menu.append((p['title'], p.path))
+    return menu
+
+
 @app.route('/')
-def home():
+def index():
     """Landing page."""
 
-    return render_template('index.html', who='magi')
+    return render_template('index.html', who='magi', navigation=menu_items())
 
 
 @app.route('/<path:path>')
 def page(path):
     page = pages.get_or_404(path)
-    return render_template('page.html', page=page)
+    return render_template('page.html', page=page, navigation=menu_items())
