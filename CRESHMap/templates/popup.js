f36723d5e709c6f2d500733f6d99863c9f9e3739
David de Klerk <dawiedotcom@gmail.com>
2023-01-19 14:31:28 +0000

    Update mapserver config file generation to new data model

--- CRESHMap/templates/popup.js
+++ CRESHMap/templates/popup.js
@@ -3,8 +3,10 @@
     "name": "[name]",
     "attributes": {
 	{% for a in attributes %}
-	"{{ a }}": {"name": "{{ attributes[a]['name'] }}",
-		    "value":  "[item name="{{ a }}" precision = "2"]"}{% if not loop.last %},{% endif %}
+	"{{ a }}": {
+      "name": "{{ attributes[a]['name'] }}",
+      "year": "{{ attributes[a]['year'] }}",
+		  "value":  "[item name="{{ a }}" precision = "2"]"}{% if not loop.last %},{% endif %}
 	{% endfor %}
     }
 }
