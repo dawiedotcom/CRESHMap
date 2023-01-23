4a2b8c6ef36278566276087367887c4d1ff1470d
David de Klerk <dawiedotcom@gmail.com>
2023-01-23 10:47:05 +0000

    Add manual break values for colormaps

--- CRESHMap/load_variables.py
+++ CRESHMap/load_variables.py
@@ -8,9 +8,10 @@ from yaml.loader import SafeLoader
 import pandas
 import colorbrewer
 
-def quantile_color_map(cmap_name, values, nbins=5):
-    def to_str(rgb):
-        return f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}'
+def color_to_str(rgb, alpha=200):
+    return f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}{alpha:02x}'
+
+def quantile_color_map(cmap_name, values, nbins=5, reverse_colors=False):
     # Result buffer
     result = pandas.DataFrame(['']*values.size, columns=['color'])
     # Find the data range. Note, some values are nonsensical such as negative numbers or NaN.
@@ -20,17 +21,38 @@ def quantile_color_map(cmap_name, values, nbins=5):
     # Create color labels
     cmap = getattr(colorbrewer, cmap_name)
     cmap = cmap[nbins]
-    cmap = list(map(to_str, cmap)) # Convert list rgb tuples to hex format strings
+    cmap = list(map(color_to_str, cmap)) # Convert list rgb tuples to hex format strings
+    if reverse_colors:
+        cmap.reverse()
     # Assign colors for interval limits
     result.loc[values <= v_min, 'color'] = cmap[0]
     result.loc[values == v_max, 'color'] = cmap[-1]
     # Assign colors according to quantiles everywhere else. Limits are not included, because
     # for some quantities the vast majority of values can be at the limits.
-    result.loc[(values < v_max) & (values > v_min), 'color'] = pandas.qcut(
-        values[(values < v_max) & (values > v_min)],
+    result.loc[(values <= v_max) & (values > v_min), 'color'] = pandas.qcut(
+        values[(values <= v_max) & (values > v_min)],
         nbins,
         labels=cmap
     )
+    return result
+
+def manual_color_map(cmap_name, values, bin_values=[], reverse_colors=False):
+    nbins = len(bin_values) - 1
+    if nbins > 9:
+        print('Colorbrewer support at most 9 different colors')
+        nbins = 9
+        print('  The following bins will be ignored:', bin_values[nbins:])
+    # Create color labels
+    cmap = getattr(colorbrewer, cmap_name)
+    cmap = cmap[nbins]
+    cmap = list(map(color_to_str, cmap))
+    if reverse_colors:
+        cmap.reverse()
+    # Find values in the specified intervals
+    result = pandas.DataFrame(['']*values.size, columns=['color'])
+    for i in range(nbins):
+        idx = (values >= bin_values[i]) & (values < bin_values[i+1])
+        result.loc[idx, 'color'] = cmap[i]
     return result
 
 def main():
@@ -82,10 +104,24 @@ def main():
 
             colormethod = variable.get('colormethod', '')
             if colormethod == "quantile":
+                if not "nclasses" in variable:
+                    print("nclasses should be specified for quantile colormothed")
+                    exit(1)
                 values["color"] = quantile_color_map(
                     variable["colormap"],
                     values["value"].to_numpy(),
-                    nbins=variable["nclasses"]
+                    nbins=variable["nclasses"],
+                    reverse_colors=variable.get("reverse_color", False)
+                )
+            elif colormethod == "manual":
+                if not "break_values" in variable:
+                    print("{db_var}: break_values should be specified for manual colormethod".format(**variable))
+                    exit(1)
+                values["color"] = manual_color_map(
+                    variable["colormap"],
+                    values["value"].to_numpy(),
+                    bin_values=variable["break_values"],
+                    reverse_colors=variable.get("reverse_color", False)
                 )
             else:
                 print('Color method {0} not supported.'.format(colormethod))
