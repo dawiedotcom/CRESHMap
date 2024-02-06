de87055a72a7e91595b053c1055e9d7ce9ef5e47
David de Klerk <david.deklerk@ed.ac.uk>
2024-02-06 13:15:10 +0000

    Update xmlx filename for the download link

--- CRESHMap/routes.py
+++ CRESHMap/routes.py
@@ -228,7 +228,7 @@ def download_data(download_hash):
 
     return send_from_directory(
         'data',
-        'Creshmap_Dataset_Alc_Tob_Gam_2023.xlsx',
+        'Creshmap_Dataset_Alc_Tob_Gam_Foo_Sup_2024.xlsx',
     )
 
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 name = 'CRESHMap'
-version = '1.0.9'
+version = '1.0.10'
 author = 'Magnus Hagdorn'
 
 setup(
